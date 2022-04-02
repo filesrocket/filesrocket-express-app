3406b07de4a4641772e0aff6ad2e2d37e0fc5bc0
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2022-04-02 13:32:38 -0600

    Cloudinary and AmazonS3 services are commented

--- src/filesrocket.ts
+++ src/filesrocket.ts
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { LocalFileService } from "filesrocket-local";
-import { AmazonS3FileService } from "filesrocket-amazons3";
-import { CloudinaryFileService } from "filesrocket-cloudinary";
+// import { AmazonS3FileService } from "filesrocket-amazons3";
+// import { CloudinaryFileService } from "filesrocket-cloudinary";
 
 const filesrocket = new Filesrocket();
 
@@ -15,21 +15,21 @@ filesrocket.register("local", new LocalFileService({
   host: "http://localhost:3030"
 }));
 
-filesrocket.register("cloudinary", new CloudinaryFileService({
-  pagination: { default: 15, max: 50 },
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-}));
+// filesrocket.register("cloudinary", new CloudinaryFileService({
+//   pagination: { default: 15, max: 50 },
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET
+// }));
 
-filesrocket.register("amazons3", new AmazonS3FileService({
-  Pagination: { default: 15, max: 50 },
-  Bucket: process.env.AMAZONS3_BUCKET as string,
-  region: process.env.AMAZONS3_REGION as string,
-  credentials: {
-    accessKeyId: process.env.AMAZONS3_ACCESS_KEY_API as string,
-    secretAccessKey: process.env.AMAZONS3_SECRET_ACCESS_KEY as string
-  }
-}));
+// filesrocket.register("amazons3", new AmazonS3FileService({
+//   Pagination: { default: 15, max: 50 },
+//   Bucket: process.env.AMAZONS3_BUCKET as string,
+//   region: process.env.AMAZONS3_REGION as string,
+//   credentials: {
+//     accessKeyId: process.env.AMAZONS3_ACCESS_KEY_API as string,
+//     secretAccessKey: process.env.AMAZONS3_SECRET_ACCESS_KEY as string
+//   }
+// }));
 
 export default filesrocket;
