a49f00dfd57550da78a26bf52379ffb3966517dc
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2022-08-19 22:33:51 -0600

    updated dependencies

--- src/filesrocket.ts
+++ src/filesrocket.ts
@@ -1,6 +1,7 @@
 import { Filesrocket, Pagination } from "@filesrocket/core";
-import { CloudinaryFileService } from "@filesrocket/cloudinary";
-import { LocalFileService } from "@filesrocket/local";
+import { CloudinaryService } from "@filesrocket/cloudinary";
+import { AmazonS3Service } from "@filesrocket/amazons3";
+import { LocalService } from "@filesrocket/local";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -9,27 +10,27 @@ const filesrocket = new Filesrocket();
 
 const PAGINATION: Pagination = { default: 15, max: 50 };
 
-filesrocket.register("local", new LocalFileService({
+filesrocket.register("local", new LocalService({
   pagination: PAGINATION,
   directory: "uploads",
   host: "http://localhost:3030"
 }));
 
-filesrocket.register("cloudinary", new CloudinaryFileService({
-  pagination: { default: 15, max: 50 },
+filesrocket.register("cloudinary", new CloudinaryService({
+  pagination: PAGINATION,
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 }));
 
-// filesrocket.register("amazons3", new AmazonS3FileService({
-//   Pagination: { default: 15, max: 50 },
-//   Bucket: process.env.AMAZONS3_BUCKET as string,
-//   region: process.env.AMAZONS3_REGION as string,
-//   credentials: {
-//     accessKeyId: process.env.AMAZONS3_ACCESS_KEY_API as string,
-//     secretAccessKey: process.env.AMAZONS3_SECRET_ACCESS_KEY as string
-//   }
-// }));
+filesrocket.register("amazons3", new AmazonS3Service({
+  Pagination: PAGINATION,
+  Bucket: process.env.AMAZONS3_BUCKET as string,
+  region: process.env.AMAZONS3_REGION as string,
+  credentials: {
+    accessKeyId: process.env.AMAZONS3_ACCESS_KEY_API as string,
+    secretAccessKey: process.env.AMAZONS3_SECRET_ACCESS_KEY as string
+  }
+}));
 
 export default filesrocket;
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,19 @@ import express from "express";
 import path from "path";
 
 import localRoutes from "./apis/local.routes";
-import cloudinaryRoutes from "./apis/cloudinary.routes";
+// import cloudinaryRoutes from "./apis/cloudinary.routes";
+// import amazonRoutes from "./apis/amazons3.routes";
 
 const app = express();
 
 // Register all endpoints.
 app.use(localRoutes);
-app.use(cloudinaryRoutes);
+// app.use(cloudinaryRoutes);
+// app.use(amazonRoutes);
 
 // Expose static files.
 app.use("/uploads", express.static(path.resolve("uploads")));
 
-app.listen(3030, () => console.log("Running..."));
+app.listen(3030, () => {
+  console.log("Execute app in http://localhost:3030")
+});
