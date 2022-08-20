2dbe3ed32b9ff6d5e90d5c1c1ef4a57c3447177d
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2022-08-19 23:39:18 -0600

    Unused services were commented

--- src/filesrocket.ts
+++ src/filesrocket.ts
@@ -1,6 +1,6 @@
 import { Filesrocket, Pagination } from "@filesrocket/core";
-import { CloudinaryService } from "@filesrocket/cloudinary";
-import { AmazonS3Service } from "@filesrocket/amazons3";
+// import { CloudinaryService } from "@filesrocket/cloudinary";
+// import { AmazonS3Service } from "@filesrocket/amazons3";
 import { LocalService } from "@filesrocket/local";
 import dotenv from "dotenv";
 
@@ -16,21 +16,21 @@ filesrocket.register("local", new LocalService({
   host: "http://localhost:3030"
 }));
 
-filesrocket.register("cloudinary", new CloudinaryService({
-  pagination: PAGINATION,
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-}));
+// filesrocket.register("cloudinary", new CloudinaryService({
+//   pagination: PAGINATION,
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET
+// }));
 
-filesrocket.register("amazons3", new AmazonS3Service({
-  Pagination: PAGINATION,
-  Bucket: process.env.AMAZONS3_BUCKET as string,
-  region: process.env.AMAZONS3_REGION as string,
-  credentials: {
-    accessKeyId: process.env.AMAZONS3_ACCESS_KEY_API as string,
-    secretAccessKey: process.env.AMAZONS3_SECRET_ACCESS_KEY as string
-  }
-}));
+// filesrocket.register("amazons3", new AmazonS3Service({
+//   Pagination: PAGINATION,
+//   Bucket: process.env.AMAZONS3_BUCKET as string,
+//   region: process.env.AMAZONS3_REGION as string,
+//   credentials: {
+//     accessKeyId: process.env.AMAZONS3_ACCESS_KEY_API as string,
+//     secretAccessKey: process.env.AMAZONS3_SECRET_ACCESS_KEY as string
+//   }
+// }));
 
 export default filesrocket;
